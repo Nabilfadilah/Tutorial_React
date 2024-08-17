0799fee05ffee3c15e581615d1592f07c9225785
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-08-17 08:25:20 +0700

    6. React Router

--- my-react-app/src/pages/login.jsx
+++ my-react-app/src/pages/login.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import AuthLayouts from "../components/layouts/AuthLayouts";
 import FormLogin from "../components/fragments/FormLogin";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <div>
       <AuthLayouts title="Login">
         <FormLogin />
+        <p className="text-sm mt-3 text-center">
+          Anda belum memiliki akun?{" "}
+          <Link to="/register" className="font-bold text-blue-600">
+            {" "}
+            Sign Up
+          </Link>
+        </p>
       </AuthLayouts>
     </div>
   );
--- my-react-app/src/pages/register.jsx
+++ my-react-app/src/pages/register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AuthLayouts from "../components/layouts/AuthLayouts";
 import FormRegister from "../components/fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
@@ -8,6 +9,14 @@ const RegisterPage = () => {
       <AuthLayouts title="Register">
         {/* children */}
         <FormRegister />
+        <p className="text-sm mt-3 text-center">
+          Sudah memiliki akun?{" "}
+          {/* client side routing, tidak memanggil dulu ke server dokumen page nya*/}
+          <Link to="/login" className="font-bold text-blue-600">
+            {" "}
+            Login
+          </Link>
+        </p>
       </AuthLayouts>
     </div>
   );
